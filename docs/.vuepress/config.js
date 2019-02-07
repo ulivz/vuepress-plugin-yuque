bcb9be700b21bc6c9608b8a9a23f7d099b5e61fe
ULIVZ <472590061@qq.com>
2019-02-08 05:11:41 +0800

    docs: update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,13 +8,13 @@ module.exports = {
         home: {
           actionText: 'Getting Started →',
           actionLink: '/intro/',
-          // heroImage: 'https://cdn.nlark.com/yuque/0/2018/png/84868/1535520500482-avatar/20c595c5-ab31-4543-9142-f36cc87c8868.png?x-oss-process=image/resize,m_fill,w_320,h_320',
+          heroImage: 'https://cdn.nlark.com/yuque/0/2019/png/242808/1549571925285-2372b0a0-0234-421c-a139-00ea16f1a106.png',
           footer: `Copyright © ULIVZ`,
-          // features: [
-          //   { title: '循序渐进', details: '本教程的难度依次递进，为阅读者呈现舒适的学习曲线' },
-          //   { title: '值得信赖', details: '由 antd 团队亲自打造，从技术栈、生态、研发流程等来为你提供系统化的学习体验' },
-          //   { title: '最佳实践', details: '通过结合实际开发过程中的案例，来描述不同场景下的最佳实践' },
-          // ]
+          features: [
+            { title: '简单', details: 'Zero-Markdown，只需配置你的语雀 repo 地址，就能获得一个 VuePress 站点' },
+            { title: '轻松', details: '自动生成的文档主页、侧边栏让你体验“超轻松”的文档生成体验' },
+            { title: '缓存', details: '完美地解决语雀 Open API 调用次数超限的问题' },
+          ]
         }
       }
     ]
